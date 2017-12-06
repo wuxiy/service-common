872f75799b9422a6dfb00197cedbc232ae90816d
wangjie <wangjie@fuhua.com>
2017-12-06 14:39:20 +0800

    添加极光推送 push 地址

--- src/main/java/com/dakun/jianzhong/utils/ServerUtils.java
+++ src/main/java/com/dakun/jianzhong/utils/ServerUtils.java
@@ -4,21 +4,27 @@ package com.dakun.jianzhong.utils;
  * Created by wangh09 on Thu Jul 06 17:15:47 CST 2017.
  */
 public class ServerUtils {
-    static final public String API_LIST_URL = "http://resource-service/api/list";
-    static final public String API_ADD_URL = "http://resource-service/api/add-batch";
-    static final public String BAIDU_LBS_POI_HOST = "http://api.map.baidu.com/geodata/v3/poi/";
-    static final public String BAIDU_GEOCODING_URL = "http://api.map.baidu.com/geocoder/v2/";
-    static final public String BAIDU_LBS_POI_CREATE = BAIDU_LBS_POI_HOST + "create";
-    static final public String BAIDU_LBS_POI_DETAIL = BAIDU_LBS_POI_HOST + "detail";
-    static final public String BAIDU_LBS_POI_LIST = BAIDU_LBS_POI_HOST + "list";
-    static final public String BAIDU_AK = "8cyBzD0qPEPHZKYuXkq01wvKmmXIioTo";
-    static final public String BAIDU_GEOTABLE = "175728";
-    static final public String COORD_TYPE = "1";
-    static final public String SOCIAL_MINE_URL = "http://social-service/mine/get";
-    static final public String RESOURCE_CROP_MINE_URL = "http://resource-service/crop/getMyConcernCrops";
-    static final public String SOCIAL_TAG_URL = "http://social-service/tag";
-    static final public String RESOURCE_PESTICIDE_URL = "http://resource-service/pesticide";
-    static final public String ACCOUNT_USER_LIST = "http://account-service/user/list";
-    static final public String ACCOUNT_USER_GET = "http://account-service/user/get";
-    static final public String ACCOUNT_USER_GET_SIMPLE = "http://account-service/user/getSimple";
+    public static final String API_LIST_URL = "http://resource-service/api/list";
+    public static final String API_ADD_URL = "http://resource-service/api/add-batch";
+    public static final String BAIDU_LBS_POI_HOST = "http://api.map.baidu.com/geodata/v3/poi/";
+    public static final String BAIDU_GEOCODING_URL = "http://api.map.baidu.com/geocoder/v2/";
+    public static final String BAIDU_LBS_POI_CREATE = BAIDU_LBS_POI_HOST + "create";
+    public static final String BAIDU_LBS_POI_DETAIL = BAIDU_LBS_POI_HOST + "detail";
+    public static final String BAIDU_LBS_POI_LIST = BAIDU_LBS_POI_HOST + "list";
+    public static final String BAIDU_AK = "8cyBzD0qPEPHZKYuXkq01wvKmmXIioTo";
+    public static final String BAIDU_GEOTABLE = "175728";
+    public static final String COORD_TYPE = "1";
+    public static final String SOCIAL_MINE_URL = "http://social-service/mine/get";
+    public static final String RESOURCE_CROP_MINE_URL = "http://resource-service/crop/getMyConcernCrops";
+    public static final String SOCIAL_TAG_URL = "http://social-service/tag";
+    public static final String RESOURCE_PESTICIDE_URL = "http://resource-service/pesticide";
+    public static final String ACCOUNT_USER_LIST = "http://account-service/user/list";
+    public static final String ACCOUNT_USER_GET = "http://account-service/user/get";
+    public static final String ACCOUNT_USER_GET_SIMPLE = "http://account-service/user/getSimple";
+    public static final String ACCOUNT_MESSAGE_SOCIAL_FOLLOW_COMMENT_PUSH = "http://account-service/message/socialFollowCommentPush";
+    public static final String ACCOUNT_MESSAGE_SOCIAL_FOLLOW_CONVERSATION_PUSH = "http://account-service/message/socialFollowConversationPush";
+    public static final String ACCOUNT_MESSAGE_SOCIAL_ATTITUDE_PUSH = "http://account-service/message/socialAttitudePush";
+    public static final String ACCOUNT_FOLLOW_LIST = "http://account-service/follow/list";
+    public static final String ACCOUNT_EXPERT_GET = "http://account-service/expert/get";
+    public static final String ACCOUNT_DEALER_GET = "http://account-service/dealer/get";
 }
